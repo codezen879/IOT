8932547c20e4d78bf982e4996b5eb3973858e9e1
codezen879 <95371909+codezen879@users.noreply.github.com>
2023-08-20 14:51:58 +0530

    Update adminHome.js

--- frontend/src/components/adminHome.js
+++ frontend/src/components/adminHome.js
@@ -22,7 +22,7 @@ export default function AdminHome({ userData }) {
 
   //fetching all user
   const getAllUser = () => {
-    fetch("http://localhost:5000/getAllUser", {
+    fetch("https://iot-2ksh.onrender.com/getAllUser", {
       method: "GET",
     })
       .then((res) => res.json())
@@ -44,7 +44,7 @@ export default function AdminHome({ userData }) {
   //deleting user
   const deleteUser = (id, name) => {
     if (window.confirm(`Are you sure you want to delete ${name}`)) {
-      fetch("http://localhost:5000/deleteUser", {
+      fetch("https://iot-2ksh.onrender.com/deleteUser", {
         method: "POST",
         crossDomain: true,
         headers: {
@@ -79,7 +79,7 @@ export default function AdminHome({ userData }) {
   }
 
   function getPaginatedUsers(){
-    fetch(`http://localhost:5000/paginatedUsers?page=${currentPage.current}&limit=${limit}`, {
+    fetch(`https://iot-2ksh.onrender.com/paginatedUsers?page=${currentPage.current}&limit=${limit}`, {
       method: "GET",
     })
       .then((res) => res.json())
